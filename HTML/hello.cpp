dadde64575e6dd3fb10dae8b30177d91972fb09e
Harshitagarwalthal <85787343+Harshitagarwalthal@users.noreply.github.com>
2021-12-08 09:29:41 +0530

    Add files via upload

---
+++ HTML/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter the first number: " << endl;
+    cin >> a;
+    double b;
+    cout << " Enter the second number: " << endl;
+    cin >> b;
+    float c = a + b;
+    cout << c << endl;
+}
